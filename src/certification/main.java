2f2aa01505cf1b39fc1ec10dd5f4db79034db889
Leonardo Medina <leoz.31@hotmail.com>
2026-01-18 17:30:41 -0600

    feat(cert): init

---
+++ src/certification/main.java
@@ -0,0 +1,10 @@
+package certification;
+
+public class main {
+    public static void main(String[] args) {
+        String a = "Leo";
+        String b = " Leo ".trim();
+
+        System.out.println( a == b );
+    }
+}
